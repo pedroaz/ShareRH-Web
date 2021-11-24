3964ec0a0ceec7033720d6dcf25ad1f63f806440
pedroaz <pedro.azvm@gmail.com>
2021-11-24 16:20:01 -0300

    Adding sample azure function

---
+++ PetBook.Backend/Petbook.AzureFunction/Petbook.AzureFunction/Function1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Petbook.AzureFunction
+{
+    public static class Function1
+    {
+        [FunctionName("test")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("Nossa Azure Function está funcionando!");
+        }
+    }
+}
